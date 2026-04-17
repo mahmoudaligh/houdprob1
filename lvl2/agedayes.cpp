fcbbf9390d68d0a803b133e777daf8ab667fb0f3
7ooda <eh@hooda.local>
2026-04-17 08:59:23 +0200

    edit prob dayes

--- lvl2/agedayes.cpp
+++ lvl2/agedayes.cpp
@@ -4,18 +4,11 @@ using namespace  std;
 int main(){
 
     int x , y , z , h , m ; cin>>x;
-
-
     y = x / 365;       
     z = x % 365;       
     h = z / 30;         
     m = z % 30;         
-
-
     cout << y << " years\n";
     cout << h << " months\n" ;
     cout << m  << " day\n";
-
-return 0;
-
 }
