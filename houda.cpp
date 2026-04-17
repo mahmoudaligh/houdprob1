5f2e7d2b803af968fc71531babe5f2a30277ca0d
7ooda <eh@hooda.local>
2026-04-17 02:14:49 +0200

    houda1

---
+++ houda.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace  std;
+
+int main(){
+
+    int x;
+    cout<< "enter ur age";
+    cin >> x;
+
+    if(x > 20){
+        cout<< "good";
+    }
+     else{
+        cout << "not good";
+     }
+     
+       
+}
+    
