e2565bc9dcdf9c263ed35c86a9764742b14c2d70
7ooda <eh@hooda.local>
2026-04-18 07:06:58 +0200

    float or int

--- brothers.cpp
+++ brothers.cpp
@@ -3,24 +3,20 @@ using namespace  std;
 
 int main(){
 
-    // string F1 , S1 ; cin >> F1 ; cin >> S1;
-    // string F2 , S2 ; cin >> F2 ; cin >> S2;
+    string F1 , S1 ; cin >> F1 ; cin >> S1;
+    string F2 , S2 ; cin >> F2 ; cin >> S2;
 
-    // if(S1 == S2)
-    // cout << "Are Brothers";
-    // else
-    // cout << "Not Brothers";
-
-
-                    string person1[2];
-                    string person2[2];
-                    cin >> person1[0] , cin >> person1[1];
-                    cin >> person2[0] , cin >> person2[1];
-
-                    if(person1[1]  == person2[1])
-                        cout << "Are Brothers";
-
-                    else
-                        cout << "Not Brothers";
+    if(S1 == S2)
+    cout << "Are Brothers";
+    else
+    cout << "Not Brothers";
 }
-    
+                        // string person1[2];
+                    // string person2[2];
+                    // cin >> person1[0] , cin >> person1[1];
+                    // cin >> person2[0] , cin >> person2[1];
+
+                    // if(person1[1]  == person2[1])
+                    //     cout << "Are Brothers";
+                    // else
+                    //     cout << "Not Brothers";
---
+++ lvl2/floatorint.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    double x ; cin >> x;
+
+if (x == (int)x )
+{
+    cout << "int " << x;
+}
+else if (x == (float)x)
+{
+    cout << "float" << x;
+}
+else    
+    cout << "thx";
+}
+
