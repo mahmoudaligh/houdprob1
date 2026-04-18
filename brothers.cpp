e18a7735da26bab2f9d8b7eb3952bb4d3bdaff2d
7ooda <eh@hooda.local>
2026-04-18 03:07:26 +0200

    array brothers

--- brothers.cpp
+++ brothers.cpp
@@ -3,13 +3,24 @@ using namespace  std;
 
 int main(){
 
-    string F1 , F2 ; cin >> F1 ; cin >> F2;
-    string S1 , S2 ; cin >> S1 ; cin >> S2;
+    // string F1 , S1 ; cin >> F1 ; cin >> S1;
+    // string F2 , S2 ; cin >> F2 ; cin >> S2;
 
-    if(F2 == S2)
-    cout << "Are Brothers";
-    else
-    cout << "Not Brothers";
+    // if(S1 == S2)
+    // cout << "Are Brothers";
+    // else
+    // cout << "Not Brothers";
 
+
+                    string person1[2];
+                    string person2[2];
+                    cin >> person1[0] , cin >> person1[1];
+                    cin >> person2[0] , cin >> person2[1];
+
+                    if(person1[1]  == person2[1])
+                        cout << "Are Brothers";
+
+                    else
+                        cout << "Not Brothers";
 }
     
